a1479ebeeeea3123caf4da09e94316a4d039cd31
Jens Heuschkel <jens@jensheuschkel.de>
2013-08-07 16:26:51 +0200

    add new vsim_gl script

--- sources/scripts/vsim_gl.tcl
+++ sources/scripts/vsim_gl.tcl
@@ -13,14 +13,14 @@
 #			VSIM $$> do ../scripts/gate_level_simulation
 
 #create a seperate library for std. cells and fill it (only needed once)
-vlib umc13
-vlog -work umc13 /cad/umc/UMC13/UMCE13H210D3_1.2/verilog_simulation_models/*
+vlib lib65
+vlog -work lib65 /cad/synopsys/libs/Faraday_UMC_65nm/Standard_Cell/tcbn65lp_200c_FE/TSMCHOME/digital/Front_End/verilog/tcbn65lp_200a/tcbn65lp.v
 
 #compile gate-level netlist file and testbench
-vlog -sv -work work ../mapped/cpu_gl.v ../tb/memory.sv ../tb/testbench.sv 
+vlog -sv -work work ../../designs/mapped/cpu_gl.v ../../sources/testbench/memory.v ../../sources/testbench/testbench.v 
 
 #simulate testbench (link to standart cell library, annotate typical timing to Device Under Test)
-vsim -L umc13 -sdftyp /testbench/cpu_i=../mapped/timing.sdf -novopt work.testbench
+vsim -L lib65 -sdftyp /testbench/cpu_i=../../designs/mapped/cpu_gl.sdf -novopt work.testbench
 
 #add signals to waveform
 add wave sim:/testbench/*
