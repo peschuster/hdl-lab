2dafd8f3ff80a91ad228f5acdec28355c28e4e42
Jens Heuschkel <jens@jensheuschkel.de>
2013-08-08 11:09:08 +0200

    sync

--- work/vsim/work/addr_ctrl_tb/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity addr_ctrl_tb is
-    generic(
-        MEM_DEPTH       : integer := 4096
-    );
-    attribute mti_svvh_generic_type : integer;
-    attribute mti_svvh_generic_type of MEM_DEPTH : constant is 1;
-end addr_ctrl_tb;
