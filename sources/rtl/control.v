5e0994e5e51fcbd502d90b2e7ce17b04776825ca
HDL Lab 2013 - Group 6 <group6@hdllab2013>
2013-08-09 10:14:48 +0200

    Implemented control for mem_wr_mode and mem_rd_mode.

--- sources/rtl/control.v
+++ sources/rtl/control.v
@@ -13,14 +13,16 @@ module control(
   o_alu_sel,
   o_addr_rd_r,
   o_registers_rd_en_r,
-  o_addr_mode
+  o_addr_mode,
+  o_mem_rd_mode_r,
+  o_mem_wr_mode_r
 );
 
 input   logic           clk, rst;
 input   logic[15:0]     i_ir_id;
 input   logic[ 3:0]     i_apsr;
 
-output  logic[ 1:0]     o_addr_mode;
+output  logic[ 1:0]     o_addr_mode, o_mem_rd_mode_r, o_mem_wr_mode_r;
 output  logic[ 2:0]     o_alu_sel;
 output  logic[ 3:0]     o_addr_rd_r;
 output  logic           o_stall_id, o_stall_ex, o_stall_mem, o_stall_wb, o_registers_rd_en_r;
@@ -70,7 +72,9 @@ ctrl_id ctrl_id_inst(
   
   .o_ir_ex_r (ir_ex),
   .o_alu_sel_r (o_alu_sel),
-  .o_mem_data_access_r(mem_data_access)
+  .o_mem_data_access_r(mem_data_access),
+  .o_mem_rd_mode_r(o_mem_rd_mode_r),
+  .o_mem_wr_mode_r(o_mem_wr_mode_r)
 );
 
 
